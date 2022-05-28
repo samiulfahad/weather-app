274247e587ac22cab7f708c016bab1ce00bd53a2
Samiul Fahad <samiul_fahad@yahoo.com>
2022-05-29 00:11:05 +0600

    Added About PAge

--- src/functions/temp.js
+++ src/functions/temp.js
@@ -15,15 +15,12 @@ const temp = (err, data, mainRes) => {
         }else{
             axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${data.lat}&lon=${data.lon}&units=metric&appid=e1bc1884bc3e485ff4bdaac7cc8963e0`)
             .then(res=>{
-            let sunRise = new Date( res.data.sys.sunrise *1000).toLocaleString('en-US', { hour: 'numeric', minute:'numeric', hour12: true})
-            let sunSet = new Date( res.data.sys.sunset *1000).toLocaleString('en-US', { hour: 'numeric', minute:'numeric', hour12: true })
-            //document.write(myDate.toGMTString()+"<br>"+myDate.toLocaleString());
             return mainRes.send({
                 temp: res.data.main.temp,
                 feels: res.data.main.feels_like,
                 place: data.place_name,
-                sunRise,
-                sunSet
+                sunrise: new Date( res.data.sys.sunrise *1000).toLocaleString('en-US', { hour: 'numeric', minute:'numeric', hour12: true}),
+                sunset:  new Date( res.data.sys.sunset *1000).toLocaleString('en-US', { hour: 'numeric', minute:'numeric', hour12: true})
             })
         
             })
